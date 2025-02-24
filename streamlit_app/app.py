d59e234f1e1ef32d732daa3df583d2e3175a8131
gk38 <86232958+gk38@users.noreply.github.com>
2025-02-24 20:59:33 +0100

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -37,7 +37,8 @@ TABS = OrderedDict(
 
 def run():
     st.sidebar.image(
-        "https://dst-studio-template.s3.eu-west-3.amazonaws.com/logo-datascientest.png",
+        "logo_cfe.jpg",
+        #"https://dst-studio-template.s3.eu-west-3.amazonaws.com/logo-datascientest.png",
         width=200,
     )
     tab_name = st.sidebar.radio("",list(TABS.keys()),0)
